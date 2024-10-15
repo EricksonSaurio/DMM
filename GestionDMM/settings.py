5fdc5145e59d70f05238a92ea8e56d0284fcdbbf
EricksonSaurio <etacamb@miumg.edu.gt>
2024-10-15 16:16:16 -0600

    se cambia el correo

--- GestionDMM/settings.py
+++ GestionDMM/settings.py
@@ -169,9 +169,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'ebatz312@gmail.com'  
-EMAIL_HOST_PASSWORD = config('EMAIL_PASSWORD')
-DEFAULT_FROM_EMAIL = 'ebatz312@gmail.com'  
+EMAIL_HOST_USER = 'dmmtotonicapan@gmail.com'  
+EMAIL_HOST_PASSWORD = 'xyqv klbj hshf ezjk'
+DEFAULT_FROM_EMAIL = 'dmmtotonicapan@gmail.com'  
 EMAIL_USE_OAUTH2 = True  
 
 DEFAULT_DOMAIN = 'https://pdrh02gj-8000.use2.devtunnels.ms/'
