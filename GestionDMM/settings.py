d8a0668448b46b562f152c32f248c3693aa2f1d9
EricksonSaurio <etacamb@miumg.edu.gt>
2024-10-15 18:27:54 -0600

    Se arregla la direccion

--- GestionDMM/settings.py
+++ GestionDMM/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-xz8xw2c@a5lw*)mff3&0$j4b5*%hsa$e6*iijyng&7z#^7oho&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['54.151.13.144']
 
 
 # Application definition
